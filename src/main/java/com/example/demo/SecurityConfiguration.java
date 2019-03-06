998c1ca4853bcdf33fc9b70883a579c50109b7c1
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-03-06 11:58:15 -0500

    tried to create admin page not working

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,20 +20,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER','ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll();
+                .formLogin().loginPage("/login").permitAll()
+        .and()
+        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login").permitAll();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("user")
+        auth.inMemoryAuthentication()
+                .withUser("victor").password(passwordEncoder().encode("phimphachanh")).authorities("ADMIN")
+                .and()
+                .withUser("user")
                 .password(passwordEncoder().encode("password")).authorities("USER")
                 .and()
                 .withUser("dave").password(passwordEncoder().encode("begreat")).authorities("USER")
                 .and()
-                .withUser("jennifer").password(passwordEncoder().encode("12345")).authorities("USER")
-                .and()
-                .withUser("victor").password(passwordEncoder().encode("phimphachanh")).authorities("USER");
+                .withUser("jennifer").password(passwordEncoder().encode("12345")).authorities("USER");
     }
 
 }
