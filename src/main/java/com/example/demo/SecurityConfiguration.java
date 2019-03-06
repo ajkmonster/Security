a7a6301c7cdc6c3afd4ddb91d4f5b47256cbc821
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-03-06 10:11:40 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("user")
+                .password(passwordEncoder().encode("password")).authorities("USER")
+                .and()
+                .withUser("dave").password(passwordEncoder().encode("begreat")).authorities("USER")
+                .and()
+                .withUser("jennifer").password(passwordEncoder().encode("12345")).authorities("USER")
+                .and()
+                .withUser("victor").password(passwordEncoder().encode("phimphachanh")).authorities("USER");
+    }
+}
